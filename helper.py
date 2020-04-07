b71fe16bca7d02b47d667a7a661650e8432f5da5
mamromer <info@romergolan.com>
2020-04-07 21:28:17 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,2 +1,5 @@
 # global parameters
 numworkers = 0
+numepochs = 300
+roboflowurl = '!curl -L https://app.roboflow.ai/ds/nw1nLjWUAM?key=4svwFCyLJM > roboflow.zip; unzip roboflow.zip; rm roboflow.zip'
+yolotype = '/cfg/yolov3-tiny.cfg'
