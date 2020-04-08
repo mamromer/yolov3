1f56c4037d2f11326f6c0b542e9d66936ef4bf48
mamromer <info@romergolan.com>
2020-04-08 09:39:42 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -3,3 +3,6 @@ numworkers = 0
 numepochs = 300
 roboflowurl = '!curl -L https://app.roboflow.ai/ds/nw1nLjWUAM?key=4svwFCyLJM > roboflow.zip; unzip roboflow.zip; rm roboflow.zip'
 yolotype = '/cfg/yolov3-tiny.cfg'
+numbatches = 64
+numaccums = 2
+imagesize = 416
